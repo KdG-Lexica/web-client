f57362a3cc1dc54c3b1febfc5c673c81be822ca8
Siemen Van de Mosselaer <siemen.vandemosselaer@student.kdg.be>
2022-02-13 23:52:16 +0100

    feat: 🎸 Added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./login/Login"
+import Viewer from "./viewer/Viewer"
+
 function App() {
   return (
-    <div>
-      Hello Documentviewer
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/viewer" element={<Viewer/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
