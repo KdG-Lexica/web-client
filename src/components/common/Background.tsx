55eaa5c81b28cdbd8ed1ebfd8b2a2fb747bcd6c5
Simon Lauwers <simon.lauwers@student.kdg.be>
2022-02-21 09:26:47 +0100

    refactor: 💡 changed bg color

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Background: FC = ({ children }) => {
     return (
-        <div className="bg-slate-200 dark:bg-black h-screen transition-all">
+        <div className="bg-slate-200 dark:bg-zinc-900 h-screen transition-all">
             {children}
         </div>
     )
