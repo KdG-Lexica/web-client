9340daa9491289862809d2a8af15e015ace55533
Simon Lauwers <simon.lauwers@student.kdg.be>
2022-03-02 00:00:07 +0100

    refactor: 💡 small refactor

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
     return (
-        <div className="bg-slate-200 dark:bg-zinc-900 h-screen transition-all">
+        <div className="bg-slate-200 dark:bg-zinc-900 h-screen">
             <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
                 <ThemeToggle />
             </div>
--- src/components/select-dataset/DatasetSelector.tsx
+++ src/components/select-dataset/DatasetSelector.tsx
@@ -22,55 +22,20 @@ export const DatasetSelector = () => {
         console.log(data);
 
         /* mock data */
-
         data = [...data, { id: "999", collectionName: "The Bible", meta: [] } as ModelType];
         setModels(data);
     };
 
-    const showSkeleton = () => {
-        return (
-            <div className="dark:bg-red h-96 border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto ">
-                <div className="animate-pulse flex space-x-4">
-                    <div className="flex-1 space-y-6 py-6">
-                        <div className="h-6 bg-slate-700 rounded col-span-1"></div>
-                        <div className="space-y-6">
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="h-3 bg-slate-700 rounded col-span-2"></div>
-                                <div className="h-3 bg-slate-700 rounded col-span-1"></div>
-                            </div>
-                            <div className="h-3 bg-slate-700 rounded"></div>
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="h-3 bg-slate-700 rounded col-span-1"></div>
-                                <div className="h-3 bg-slate-700 rounded col-span-2"></div>
-                            </div>
-                            <div className="h-3 bg-slate-700 rounded"></div>
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="h-3 bg-slate-700 rounded col-span-2"></div>
-                                <div className="h-3 bg-slate-700 rounded col-span-1"></div>
-                            </div>
-                            <div className="h-3 bg-slate-700 rounded"></div>
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="h-3 bg-slate-700 rounded col-span-1"></div>
-                                <div className="h-3 bg-slate-700 rounded col-span-2"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>)
-
-    }
-
     return (
         <div className="flex flex-col items-center h-screen justify-center">
             <p className="dark:text-white text-2xl p-4 text-zinc-800 font-bold">Choose a dataset</p>
-            {isLoading ? showSkeleton() :
-                <div className="flex flex-col md:flex-row items-center justify-center gap-4">
-                    {models.map((m) => {
-                        return (
-                            <DatasetCard key={m.id} id={m.id} name={m.collectionName} description={"2M+ datapoints collected from the New York Times. Browse through documents in a 3D space and explore different clusters.".substring(0, 150).concat("...")} />
-                        )
-                    })}
-                </div>}
+            <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+                {models.map((m) => {
+                    return (
+                        <DatasetCard key={m.id} id={m.id} name={m.collectionName} description={"2M+ datapoints collected from the New York Times. Browse through documents in a 3D space and explore different clusters.".substring(0, 150).concat("...")} />
+                    )
+                })}
+            </div>
         </div>
     )
 };
