f3520bb40f9951bd042b1b33e0f3cca54961f105
Simon Lauwers <simon.lauwers@student.kdg.be>
2022-03-04 14:50:33 +0100

    perf: ⚡️ improved appbar routing

--- src/components/viewer/appbar/Appbar.tsx
+++ src/components/viewer/appbar/Appbar.tsx
@@ -9,7 +9,7 @@ export const Appbar = () => {
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="#" className="flex items-center">
+                <a onClick={() => navigate("/")} className="flex items-center cursor-pointer">
                     <img src="/documentviewerlogo.svg" className="mt-1 mr-3 h-8 sm:h-10" alt="Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Spatial Document Viewer</span>
                 </a>
@@ -24,16 +24,16 @@ export const Appbar = () => {
                 <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
                     <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                         <li>
-                            <a href="#" onClick={() => navigate("/")} className={`block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/" || location.pathname === "/home" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Home</a>
+                            <a onClick={() => navigate("/")} className={`cursor-pointer block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/" || location.pathname === "/home" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Home</a>
                         </li>
                         <li>
-                            <a href="#" onClick={() => navigate("/viewer?size=0.25&set=1")} className={`block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/viewer" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Viewer</a>
+                            <a onClick={() => navigate("/viewer?size=0.25&set=1")} className={`cursor-pointer block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/viewer" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Viewer</a>
                         </li>
                         <li>
-                            <a href="#" onClick={() => navigate("/tutorial")} className={`block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/tutorial" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Tutorial</a>
+                            <a onClick={() => navigate("/tutorial")} className={`cursor-pointer block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/tutorial" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>Tutorial</a>
                         </li>
                         <li>
-                            <a href="#" onClick={() => navigate("/about")} className={`block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/about" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>About</a>
+                            <a onClick={() => navigate("/about")} className={`cursor-pointer block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${location.pathname === "/about" ? "md:text-blue-700 dark:text-white" : "text-gray-700 dark:text-gray-400"}`}>About</a>
                         </li>
                     </ul>
                 </div>
