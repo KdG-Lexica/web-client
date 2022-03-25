9e33d0413bc0ac5fa8032262b3ac8a5c4bac162e
Siemen Van de Mosselaer <siemen.vdmv@gmail.com>
2022-03-25 14:06:07 +0100

    feat: 🎸 Added points for words

--- src/components/viewer/three-components/default-viewer/WordMesh.tsx
+++ src/components/viewer/three-components/default-viewer/WordMesh.tsx
@@ -1,6 +1,8 @@
-import { Billboard, Html, Text } from "@react-three/drei";
-import React from "react";
+import { Billboard, Html } from "@react-three/drei";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { InstancedMesh, Euler, Quaternion, Vector3, Matrix4 } from "three";
 import IPTCType from "../../../../types/IPTCType";
+import Vector3Type from "../../../../types/Vector3Type";
 
 interface WordMeshProps {
     IPTC: IPTCType;
@@ -19,13 +21,42 @@ interface WordMeshProps {
 */
 
 const WordMesh = (props: WordMeshProps) => {
-    const size = 0.03 * props.pointSize;
+    const [data, setData] = useState<Vector3Type[]>([]);
+    
+    const meshRef = useRef<InstancedMesh>();
+
+    useEffect(() => {
+        setData(props.IPTC.meta.flatMap(chunk => chunk.vector3));
+    }, [props.IPTC.meta.length])
+
+    useLayoutEffect(() => {      
+        const rotation = new Euler();
+        const quaternion = new Quaternion();
+        const scale = new Vector3();
+
+        rotation.x = rotation.y = rotation.z = 0;
+        quaternion.setFromEuler(rotation);
+        scale.x = scale.y = scale.z = 0.3;
+        
+        for (let index = 0; index < data.length; index++) {
+            const position = new Vector3();
+            const matrix = new Matrix4();
+
+            position.x = data[index].x;
+            position.y = data[index].y;
+            position.z = data[index].z;
+            matrix.compose(position, quaternion, scale);
+
+            meshRef.current!.setMatrixAt(index, matrix);
+        }
+        meshRef.current!.instanceMatrix.needsUpdate = true;        
+    }, [data]);
 
     return (
         <>
             {props.IPTC.meta.map((word, index) => {
                 return (
-                    <Billboard key={index} position={[word.vector3.x, word.vector3.y + 0.1, word.vector3.z]}>
+                    <Billboard key={index} position={[word.vector3.x, word.vector3.y, word.vector3.z]}>
                         <Html style={{ pointerEvents: "none", zIndex: 49 }}>
                             <p style={{
                                 color: "black", WebkitTextStrokeWidth: "0.5px", WebkitTextStrokeColor: "#FFFFFF",
@@ -37,6 +68,10 @@ const WordMesh = (props: WordMeshProps) => {
                     </Billboard>
                 );
             })}
+            <instancedMesh ref={meshRef} args={[undefined, undefined, data.length]} >
+                <sphereBufferGeometry attach="geometry" args={[0.1 * props.pointSize, 16, 16]}/>
+                <meshToonMaterial attach="material" color={"green"} opacity={0.5} transparent/>
+            </instancedMesh>
         </>
     );
 }
